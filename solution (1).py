07f350f14d53b4c0d94475ab2c03197ddeddd6f8
Saimon2year <romanov7semen@ya.ru>
2021-02-10 18:04:02 +0300

    first task

--- solution (1).py
+++ solution (1).py
@@ -1,5 +1,10 @@
-import geo
-toponym_to_find = "Москва, ул. Ак. Королева, 12"
+import solution
+toponym_to_find = "Канаш Москавская 19"
 
-map_params = geo.get_span(toponym_to_find)
-geo.show_map(**map_params, map_type="sat")
+coord = 47.496938, 55.507703
+span = 0.001, 0.001
+map_params = {'ll': ",".join(map(str, coord)),
+              "spn": ",".join(map(str,span))
+}
+
+solution.show_map(**map_params, map_type="sat")
